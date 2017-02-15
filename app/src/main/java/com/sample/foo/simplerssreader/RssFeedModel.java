fd42e2780c27beb43c49ef228d6f3583b847220f
toltonj@mail.uoguelph.ca <toltonj@mail.uoguelph.ca>
2017-02-15 17:00:56 -0500

    Just pushing

--- app/src/main/java/com/sample/foo/simplerssreader/RssFeedModel.java
+++ app/src/main/java/com/sample/foo/simplerssreader/RssFeedModel.java
@@ -18,6 +18,8 @@ public class RssFeedModel {
         int x = 0;
         this.title = title;
         this.link = link;
+        String xmlChange = "";
+
         for (char c : description.toCharArray()) {
 
             if (c == '<') {
@@ -26,21 +28,32 @@ public class RssFeedModel {
                 x = 0;
             } else if (c == '&') {
                 x = 2;
+                xmlChange = xmlChange + c;
             } else if (x == 0) {
                 newDescription = newDescription + c;
             } else if (x == 2) {
-                if (c == 'q') {
-                    x = 3;
+                if (c != ';') {
+                    xmlChange = xmlChange + c;
                 } else {
-                    x = 4;
+                    x = 0;
+
+                    if (xmlChange.equals("&#8217") || xmlChange.equals("&#039")) {
+                        newDescription = newDescription + "'";
+                        xmlChange = "";
+                        x = 0;
+                    } else if (xmlChange.equals("&#8220") || xmlChange.equals("&#8221")) {
+                        newDescription = newDescription + '"';
+                        xmlChange = "";
+                        x = 0;
+                    } else if (xmlChange.equals("&nbsp")) {
+                        xmlChange = "";
+                        x = 0;
+                    } else {
+                        xmlChange = "";
+                        x = 0;
+                    }
                 }
-            } else if (c == ';') {
-                if (x == 3) {
-                    newDescription = newDescription + '"';
-                } else if (x == 4) {
-                    newDescription = newDescription + "'";
-                }
-                x = 0;
+
             }
 
         }
