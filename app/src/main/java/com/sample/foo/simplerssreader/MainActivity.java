e4e5af0f924febc6490aef97f7427ec8c3199419
Joline <joline@mail.uoguelph.ca>
2017-02-14 19:57:21 -0500

    title, description, photo match

--- app/src/main/java/com/sample/foo/simplerssreader/MainActivity.java
+++ app/src/main/java/com/sample/foo/simplerssreader/MainActivity.java
@@ -1,19 +1,31 @@
 package com.sample.foo.simplerssreader;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Xml;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -21,17 +33,21 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     private RecyclerView mRecyclerView;
     private EditText mEditText;
     private Button mFetchFeedButton;
+    private Button subFeedButton;
     private SwipeRefreshLayout mSwipeLayout;
     private TextView mFeedTitleTextView;
     private TextView mFeedLinkTextView;
@@ -41,15 +57,27 @@ public class MainActivity extends AppCompatActivity {
     private String mFeedTitle;
     private String mFeedLink;
     private String mFeedDescription;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mEditText = (EditText) findViewById(R.id.rssFeedEditText);
         mFetchFeedButton = (Button) findViewById(R.id.fetchFeedButton);
+        subFeedButton = (Button) findViewById(R.id.subFeedButton);
         mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         mFeedTitleTextView = (TextView) findViewById(R.id.feedTitle);
         mFeedDescriptionTextView = (TextView) findViewById(R.id.feedDescription);
@@ -69,13 +97,85 @@ public class MainActivity extends AppCompatActivity {
                 new FetchFeedTask().execute((Void) null);
             }
         });
+        subFeedButton.setOnClickListener(new View.OnClickListener()
+        {//IMPORTANT NOTE: READ THIS FOR ADDING NEW MENU ITEMS PROGRAMATICALLY
+            //popup.getMenu().add(groupId, itemId, order, title); for each menuItem you want to add.
+            @Override
+            public void onClick(View view)
+            {
+                PopupMenu popup = new PopupMenu(MainActivity.this,subFeedButton);
+                //Inflating the Popup through the xml file
+                popup.getMenuInflater().inflate(R.menu.subscribe_menu, popup.getMenu());
+                //registering popup with OnMenuItemClickListener. So you can click on the options
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+                {
+                    public boolean onMenuItemClick(MenuItem item)
+                    {
+                        /*
+                        Toast.makeText(
+                                MainActivity.this,
+                                "You Clicked : " + item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        */
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // menu_main shares the name of menu_main.xml Inflating the menus inside it.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.dateCreated) {
+            return true;
+        }
+        if (id == R.id.dateModified) {
+            return true;
+        }
+        if (id == R.id.articleTitle) {
+            return true;
+        }
+        if (id == R.id.Author) {
+            return true;
+        }
+        if (id == R.id.Random) {
+            return true;
+        }
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return(true);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public List<RssFeedModel> parseFeed(InputStream inputStream) throws XmlPullParserException, IOException {
         String title = null;
         String link = null;
         String description = null;
+        String thumbUrl = null;
         boolean isItem = false;
+        boolean endItem = false;
         List<RssFeedModel> items = new ArrayList<>();
 
         try {
@@ -88,19 +188,23 @@ public class MainActivity extends AppCompatActivity {
                 int eventType = xmlPullParser.getEventType();
 
                 String name = xmlPullParser.getName();
-                if(name == null)
+                if (name == null)
                     continue;
 
-                if(eventType == XmlPullParser.END_TAG) {
-                    if(name.equalsIgnoreCase("item")) {
+                if (eventType == XmlPullParser.END_TAG) {
+                    if (name.equalsIgnoreCase("item")) {
+                        Log.d("Main Activity", "end article");
                         isItem = false;
+                        endItem = true;
                     }
                     continue;
                 }
 
                 if (eventType == XmlPullParser.START_TAG) {
-                    if(name.equalsIgnoreCase("item")) {
+                    if (name.equalsIgnoreCase("item")) {
+                        Log.d("MainActivity", "start article");
                         isItem = true;
+                        //endItem = false;
                         continue;
                     }
                 }
@@ -112,29 +216,56 @@ public class MainActivity extends AppCompatActivity {
                     xmlPullParser.nextTag();
                 }
 
-                if (name.equalsIgnoreCase("title")) {
+                name = name.toLowerCase();
+                /*switch (name) {
+                    case "title":
+                        title = result;
+                        break;
+                    case "link":
+                        link = result;
+                        break;
+                    case "description":
+                        description = result;
+                        break;
+                    case "media:thumbnail":
+                        thumbUrl = xmlPullParser.getAttributeValue(null, "url");
+                        break;
+                }*/
+                if(name.equals("title") && title == null){
                     title = result;
-                } else if (name.equalsIgnoreCase("link")) {
+                }
+                else if(name.equals("link")){
                     link = result;
-                } else if (name.equalsIgnoreCase("description")) {
+                }
+                else if(name.equals("description")){
                     description = result;
                 }
+                else if(name.equals("media:thumbnail")){
+                    thumbUrl = xmlPullParser.getAttributeValue(null, "url");
+                }
+                else{
+                    continue;
+                }
 
-                if (title != null && link != null && description != null) {
-                    if(isItem) {
-                        RssFeedModel item = new RssFeedModel(title, link, description);
+                if (title != null && link != null && description != null && endItem == true) {
+                    if (isItem) {
+                        Log.d("MainActivity",title + " " + link + " "+ description + " " + thumbUrl);
+                        RssFeedModel item = new RssFeedModel(title, link, description, thumbUrl);
+                        item.printArticle();
                         items.add(item);
-                    }
-                    else {
+                    } else {
                         mFeedTitle = title;
                         mFeedLink = link;
                         mFeedDescription = description;
+                        Log.d("MainActivity", mFeedTitle + " "+ mFeedLink+ " " + mFeedDescription);
                     }
 
                     title = null;
                     link = null;
                     description = null;
+                    thumbUrl = null;
                     isItem = false;
+                    endItem = false;
                 }
             }
 
@@ -144,6 +275,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
     private class FetchFeedTask extends AsyncTask<Void, Void, Boolean> {
 
         private String urlLink;
@@ -151,9 +318,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             mSwipeLayout.setRefreshing(true);
-            mFeedTitle = null;
-            mFeedLink = null;
-            mFeedDescription = null;
+            if (!mEditText.getText().toString().matches("")) {
+                mFeedTitle = null;
+                mFeedLink = null;
+                mFeedDescription = null;
+            }
             mFeedTitleTextView.setText("Feed Title: " + mFeedTitle);
             mFeedDescriptionTextView.setText("Feed Description: " + mFeedDescription);
             mFeedLinkTextView.setText("Feed Link: " + mFeedLink);
@@ -166,12 +335,25 @@ public class MainActivity extends AppCompatActivity {
                 return false;
 
             try {
-                if(!urlLink.startsWith("http://") && !urlLink.startsWith("https://"))
-                    urlLink = "http://" + urlLink;
-
-                URL url = new URL(urlLink);
-                InputStream inputStream = url.openConnection().getInputStream();
-                mFeedModelList = parseFeed(inputStream);
+                InputStream stream;
+                Boolean hasNoProtocol = !urlLink.startsWith("http://") && !urlLink.startsWith("https://");
+                if (hasNoProtocol) {
+                    URL httpUrl = new URL("http://" + urlLink);
+                    URL httpsUrl = new URL("https://" + urlLink);
+                    try {
+                        URLConnection connection = httpsUrl.openConnection();
+                        connection.setConnectTimeout(1000);
+                        stream = connection.getInputStream();
+                    } catch (Exception e) {
+                        URLConnection connection = httpUrl.openConnection();
+                        connection.setConnectTimeout(1000);
+                        stream = connection.getInputStream();
+                    }
+                } else {
+                    URL url = new URL(urlLink);
+                    stream = url.openConnection().getInputStream();
+                }
+                mFeedModelList = parseFeed(stream);
                 return true;
             } catch (IOException e) {
                 Log.e(TAG, "Error", e);
@@ -184,7 +366,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Boolean success) {
             mSwipeLayout.setRefreshing(false);
-
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
             if (success) {
                 mFeedTitleTextView.setText("Feed Title: " + mFeedTitle);
                 mFeedDescriptionTextView.setText("Feed Description: " + mFeedDescription);
